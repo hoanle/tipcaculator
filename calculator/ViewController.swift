e3c0936206a95a5156a3262ae240b4c4d90c6199
Hoan Le <thanhhoan.le@2359media.com.vn>
2015-08-08 19:38:24 +0700

    First commit

--- calculator/ViewController.swift
+++ calculator/ViewController.swift
@@ -10,9 +10,21 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tipRate: UISegmentedControl!
+    @IBOutlet weak var tipLabel: UILabel!
+    @IBOutlet weak var billField: UITextField!
+    @IBOutlet weak var totalLable: UILabel!
+    let numberFormatter = NSNumberFormatter()
+
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        tipLabel.text = "$0.00"
+        totalLable.text = "$0.00"
+        billField.text = ""
+        billField.placeholder = "Enter money here"
+        
+        numberFormatter.numberStyle = NSNumberFormatterStyle.DecimalStyle
     }
 
     override func didReceiveMemoryWarning() {
@@ -20,6 +32,42 @@ class ViewController: UIViewController {
         // Dispose of any resources that can be recreated.
     }
 
+    @IBAction func onEditingChanged(sender: AnyObject) {
+        var billAmount = 0.0
+        
+        if (countElements(billField.text!) > 10) {
+            billField.deleteBackward()
+            billAmount = numberFormatter.numberFromString(billField.text)!
+        }else if (countElements(billField.text!) > 0){
+            billAmount = numberFormatter.numberFromString(billField.text)!
+        }else{
+            billAmount = 0.0
+        }
+        
+        var tip = billAmount * getTipPercentage()
+        var total = billAmount + tip
+        
+        tipLabel.text = String(format: "$%.2f", tip)
+        totalLable.text = String(format: "$%.2f", total)
+        
+    }
 
+    @IBAction func onTap(sender: AnyObject) {
+        view.endEditing(true)
+    }
+    
+    func getTipPercentage()-> Double{
+        switch tipRate.selectedSegmentIndex{
+            case 1:
+                return 0.20
+            case 2:
+                return 0.22
+            default:
+                return 0.18
+        }
+    }
+    @IBAction func onValueChanged(sender: AnyObject) {
+        onEditingChanged(sender)
+    }
 }
 
